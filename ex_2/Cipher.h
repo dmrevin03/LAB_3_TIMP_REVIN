6cf3497a77c6cab98d2f51168159117caced722b
dmrevin03 <112693015+dmrevin03@users.noreply.github.com>
2023-01-20 20:06:23 +0300

    Update Cipher.h

--- ex_2/Cipher.h
+++ ex_2/Cipher.h
@@ -1,32 +1,27 @@
 #pragma once
-#include <vector>
 #include <string>
-#include <map>
-#include <codecvt>
-#include <locale>
+#include <vector>
 #include <iostream>
+
+using namespace std;
 class Cipher
 {
 private:
-	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> codec;
-	int columns, rows, len_text;
+    int k;
+    wstring getValidOpenText(const std::wstring & s);
+    wstring getValidCipherText(const std::wstring & s);
+    int getValidKey(const int k, const std::wstring & s);
 public:
-	Cipher()=delete;
-	Cipher(const std::wstring & ws_key);
-	std::wstring encrypt(const std::wstring& ws_open_text);
-	std::wstring decrypt(const std::wstring& ws_cipher_text);
-	void set_tableform(const std::wstring& ws_text);
-	void set_key(std::wstring & ws_key);
-	int getValidKey(std::wstring& ws_key);
-	std::wstring getValidOpenText(const std::wstring & ws_open_text);
-	std::wstring getValidCipherText(const std::wstring & ws_cipher_text);
+    Cipher()=delete;
+    Cipher (int w);
+    wstring CoderCipher(Cipher w, wstring& s);
+    wstring DecoderCipher(Cipher w, wstring& s);
 };
-
 class cipher_error: public std::invalid_argument
 {
 public:
-	explicit cipher_error (const std::string& what_arg):
-		std::invalid_argument(what_arg) {}
-	explicit cipher_error (const char* what_arg):
-		std::invalid_argument(what_arg) {}
+    explicit cipher_error (const std::string& what_arg):
+        std::invalid_argument(what_arg) {}
+    explicit cipher_error (const char* what_arg):
+        std::invalid_argument(what_arg) {}
 };
